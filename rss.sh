e7501b95533f3e59a8708d41cc99940ef8ec59ea
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-10-22 21:00:01 +0100

    Add files via upload
    Added GOL and time of refresh.

--- rss.sh
+++ rss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-aria2c -D --dir=/home/vibri/scripts -o /resources/bbc.xml --allow-overwrite=true https://feeds.bbci.co.uk/news/video_and_audio/news_front_page/rss.xml?edition=uk
+aria2c -D --dir=/home/vibri/scripts -o /resources/bbc.xml --allow-overwrite=true https://feeds.bbci.co.uk/news/uk/rss.xml
 aria2c -D --dir=/home/vibri/scripts -o /resources/verge.xml --allow-overwrite=true https://www.theverge.com/rss/index.xml
 aria2c -D --dir=/home/vibri/scripts -o /resources/gol.xml --allow-overwrite=true https://www.gamingonlinux.com/article_rss.php
 aria2c -D --dir=/home/vibri/scripts -o /resources/ns.xml --allow-overwrite=true https://www.newscientist.com/section/news/feed/
@@ -11,22 +11,30 @@ truncate -s 0 /home/vibri/scripts/resources/rss_all.txt
 echo '-- COMMANDS q(quit) arrow keys(scroll) PGUP/PGDN /(search)  ------' >> /home/vibri/scripts/resources/rss_all.txt
 echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
 echo '-- BBC NEWS ------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
-grep -v 'BBC News - News front page' /home/vibri/scripts/resources/bbc.xml | grep "<title>"  | sed 's/            <title><!\[CDATA\[/\|- /g' | sed 's/\]\]><\/title>//g' | head -n 20 >> /home/vibri/scripts/resources/rss_all.txt
+grep '<lastBuildDate>' /home/vibri/scripts/resources/bbc.xml | sed 's/        <lastBuildDate>//g' | sed 's/<\/lastBuildDate>//g' >> /home/vibri/scripts/resources/rss_all.txt
+grep -v 'BBC News - UK' /home/vibri/scripts/resources/bbc.xml | grep "<title>"  | sed 's/            <title><!\[CDATA\[/\|- /g' | sed 's/\]\]><\/title>//g' | head -n 20 >> /home/vibri/scripts/resources/rss_all.txt
 
 echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
 echo '-- VERGE ---------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
+grep -m 1 '<updated>' /home/vibri/scripts/resources/verge.xml | sed 's/  <updated>//g' | sed 's/<\/updated>//g' >> /home/vibri/scripts/resources/rss_all.txt
 grep -v '  <title>The Verge -  All Posts' /home/vibri/scripts/resources/verge.xml | grep "<title>" | sed 's/    <title>/\|- /g' | sed 's/<\/title>//g' | head -n 20 >> /home/vibri/scripts/resources/rss_all.txt
 
 echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
 echo '-- NEW SCIENTIST -------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
+grep -m 1 '<pubDate>' /home/vibri/scripts/resources/ns.xml | sed 's/            <pubDate>//g' | sed 's/<\/pubDate>//g' >> /home/vibri/scripts/resources/rss_all.txt
 grep -v 'New Scientist - News' /home/vibri/scripts/resources/ns.xml | grep "<title>"  | sed 's/            <title>/\|- /g' | sed 's/<\/title>//g' | head -n 20 >> /home/vibri/scripts/resources/rss_all.txt
 
 echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
 echo '-- POLYGON GAMING ------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
+grep -m 1 '<updated>' /home/vibri/scripts/resources/polygon.php | sed 's/  <updated>//g' | sed 's/<\/updated>//g' >> /home/vibri/scripts/resources/rss_all.txt
 grep -v '  <title>Polygon -  All' /home/vibri/scripts/resources/polygon.php | grep "<title>" | sed 's/    <title>/\|- /g' | sed 's/<\/title>//g' | head -n 20 >> /home/vibri/scripts/resources/rss_all.txt
 
 echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
-#echo '-- GAMING ON LINUX -----------------------------------------------'
-#grep "<title>" /home/vibri/scripts/rss/gol.xml | sed 's/            <title><!\[CDATA\[/\|-/g' | sed 's/\]\]><\/title>//g' | head -n 27 | tail -n 25 | fold -s -w 80
+echo '-- GAMING ON LINUX -----------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
+sed -e 's/<description>/\
+|- /g' -e 's/<\/description>/\
+/g' < /home/vibri/scripts/resources/gol.xml | grep -v '<' | grep -v 'The latest articles from GamingOnLinux' | sed 's/&quot;//g' | head -n 8  >> /home/vibri/scripts/resources/rss_all.txt
+echo '------------------------------------------------------------------' >> /home/vibri/scripts/resources/rss_all.txt
+
 
 fold -s -w80 /home/vibri/scripts/resources/rss_all.txt | less
