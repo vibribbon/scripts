864ed484b9f6ddbaf577baf26480eb9f32cd3137
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-12-05 14:23:05 +0000

    Add files via upload

--- files_accessed_day.sh
+++ files_accessed_day.sh
@@ -2,4 +2,5 @@
 
 # Find all recently accessed files within the last day
 # format \n is new line between ites, %AF = yyyy-mm-dd, %AR = hh-mm, %p is filename.
-find /home -type f -newermt "1 day ago" -printf "\n%AF %AR | %p" 2>/dev/null | less -i
+find /home -type f -newermt "1 day ago" -printf "\n%AF %AT. | %p" 2>/dev/null | sed 's/\.[^\.]*\.//' | sort -r | less -i
+
--- files_modified_day.sh
+++ files_modified_day.sh
@@ -2,4 +2,4 @@
 
 # Find all recently modified files within the last day
 # format \n is new line between ites, %AF = yyyy-mm-dd, %AR = hh-mm, %p is filename.
-find /home -type f -newerat "1 day ago" -printf "\n%AF %AR | %p" 2>/dev/null | less
+find /home -type f -newerat "1 day ago" -printf "\n%AF %AT. | %p" 2>/dev/null | sed 's/\.[^\.]*\.//' | sort -r | less -i
