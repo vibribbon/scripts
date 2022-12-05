0497f30adac895c556f267681204557638e64b28
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-12-05 14:40:42 +0000

    Add files via upload

--- bak_inssserver.sh
+++ bak_inssserver.sh
@@ -2,17 +2,18 @@
 
 # backup the insurgency sandstorm server
 
-mkdir /home/vibri/downloads/bak_inssserver
-mkdir /home/vibri/downloads/bak_inssserver/serverfiles
-mkdir /home/vibri/downloads/bak_inssserver/serverfiles/Insurgency
-mkdir /home/vibri/downloads/bak_inssserver/serverfiles/Insurgency/Config
-mkdir /home/vibri/downloads/bak_inssserver/serverfiles/Insurgency/Saved
-mkdir /home/vibri/downloads/bak_inssserver/lgsm
+# Create a file structure
+mkdir ~/downloads/bak_inssserver
+mkdir ~/downloads/bak_inssserver/serverfiles
+mkdir ~/downloads/bak_inssserver/serverfiles/Insurgency
+mkdir ~/downloads/bak_inssserver/serverfiles/Insurgency/Config
+mkdir ~/downloads/bak_inssserver/serverfiles/Insurgency/Saved
+mkdir ~/downloads/bak_inssserver/lgsm
 
-rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/serverfiles/Insurgency/Config/ /home/vibri/downloads/bak_inssserver/serverfiles/Insurgency/Config/
-rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/serverfiles/Insurgency/Saved/ /home/vibri/downloads/bak_inssserver/serverfiles/Insurgency/Saved/
-rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/lgsm/ /home/vibri/downloads/bak_inssserver/lgsm
+rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/serverfiles/Insurgency/Config/ ~/downloads/bak_inssserver/serverfiles/Insurgency/Config/
+rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/serverfiles/Insurgency/Saved/ ~/downloads/bak_inssserver/serverfiles/Insurgency/Saved/
+rsync -avP --delete -e ssh inssserver@192.168.1.21:/home/inssserver/lgsm/ ~/downloads/bak_inssserver/lgsm
 
-7z a /home/vibri/downloads/bak_inssserver.7z /home/vibri/downloads/bak_inssserver/* -mx=9
+7z a ~/downloads/bak_inssserver.7z ~/downloads/bak_inssserver/* -mx=9
 
-rm -r /home/vibri/downloads/bak_inssserver
+rm -r ~/downloads/bak_inssserver
