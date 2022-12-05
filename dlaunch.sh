4a0196de2f9d11e5ce770ad719ecc44dcbb779c6
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-12-05 22:09:08 +0000

    Add files via upload

--- dlaunch.sh
+++ dlaunch.sh
@@ -5,7 +5,11 @@
 # If there is a single parameter then add it as a bookmark, otherwise run the menu.
 # Hold shift and enter to force what you type in the bar instead.
 touch ~/dlaunch.txt
-
 dl=$(cat ~/dlaunch.txt | dmenu -i -l 40 -sb '#495F41' -sf '#00FDDC' -p "Command (S-RTN override):")
-echo $dl >> ~/dlaunch.txt
-uxterm -fa default -fs 12 -e $dl
+
+uxterm -fa default -fs 12 -hold -e $dl
+if grep -q $dl ~/dlaunch.txt; then
+    :
+else
+    echo $dl >> ~/dlaunch.txt
+fi
