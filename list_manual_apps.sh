ec94cc2a412d5c28b5f477158cb6f9d254b0cee7
vibribbon <109892129+vibribbon@users.noreply.github.com>
2023-08-20 21:53:53 +0100

    Delete list_manual_apps.sh

--- list_manual_apps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# List all manually installed apps
-sudo grep -oP "Unpacking \K[^: ]+" /var/log/installer/syslog \
-  | sort -u | comm -13 /dev/stdin <(apt-mark showmanual | sort) | less -i
